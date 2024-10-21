8933b32fab75c041d432cb7a3aabe54515bcb161
masendhy <masendhy@masendhys-MacBook-Pro.local>
2024-10-21 16:16:46 +0700

    frontend template

---
+++ zullkit-html/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html'],
+  },
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        navy: '#160442'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
